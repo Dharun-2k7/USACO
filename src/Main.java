2d6ed61fa326f2af14d9741998daba8dd4940073
dharunkaarthick <dharunkaarthick07@gmail.com>
2025-07-21 02:15:33 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int m= sc.nextInt(),n=sc.nextInt(),k= sc.nextInt();
+        sc.nextLine();
+        for(int i=0;i<m;i++){
+            String s=sc.nextLine();
+            StringBuilder big= new StringBuilder();
+            for(int j=0;j<n;j++){
+                char c=s.charAt(j);
+                for(int l=0;l<k;l++){
+                    big.append(c);
+                }
+            }
+            for(int x=0;x<k;x++){
+                System.out.println(big);
+            }
+        }
+    }
+}
